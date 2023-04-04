21bbe19e2052035ac7d2512b5a44ff38c6526298
Guilherme Monte <gmonte@inf.ufsm.br>
2023-04-04 02:12:06 -0300

    feat: added investment history modal

--- packages/web/src/modules/investment/modals/InvestmentHistoryModal.tsx
+++ packages/web/src/modules/investment/modals/InvestmentHistoryModal.tsx
@@ -1,13 +1,23 @@
 import { useMemo } from 'react'
+import ApexChart from 'react-apexcharts'
+import { renderToString } from 'react-dom/server'
 
-import { LineChart } from 'amazing-react-charts'
+import map from 'lodash/map'
+import moment from 'moment'
 import { CircleNotch } from 'phosphor-react'
 
-import { Button } from '~/components/Button'
 import { FetchErrorMessage } from '~/components/FetchErrorMessage'
+import { Growth } from '~/components/Growth'
 import { Modal } from '~/components/Modal'
+import { Text } from '~/components/Text'
 import { ModalProps } from '~/hooks/useModal'
 import { api } from '~/services/api'
+import {
+  formatCurrency,
+  formatNumber
+} from '~/utils/formatters'
+
+import { TooltipItem } from '../components/TooltipItem'
 
 export interface InvestmentHistoryModalProps extends ModalProps {
   investmentId: string
@@ -35,161 +45,195 @@ export function InvestmentHistoryModal({
     [errorGetCdiInvestmentHistory, isErrorGetCdiInvestmentHistory]
   )
 
-  console.log('data', data)
+  const options = useMemo<ApexCharts.ApexOptions>(
+    () => ({
+      chart: {
+        background: 'transparent',
+        animations: { enabled: false },
+        toolbar: { show: false },
+        zoom: { enabled: false }
+      },
+      colors: ['#c18e1f', '#3dbcaf', '#764184', '#2290de'],
+      theme: { mode: 'dark' },
+      dataLabels: { enabled: false },
+      stroke: { curve: 'straight' },
+      xaxis: {
+        type: 'category',
+        categories: map(data?.history, item => moment(item.date).format('L')),
+        axisTicks: { show: false },
+        labels: { show: false }
+      },
+      yaxis: {
+        labels: { formatter: formatCurrency },
+        showForNullSeries: false
+      },
+      tooltip: {
+        custom({ dataPointIndex, w }: { dataPointIndex: number, w: { globals: { categoryLabels: string[], colors: string[], seriesNames: string [] } }}) {
+          const historyItem = data?.history?.[dataPointIndex]
+          const label = w.globals.categoryLabels[dataPointIndex]
+          return renderToString(
+            <>
+              <div className="apexcharts-tooltip-title">
+                {label}
+              </div>
+
+              <div className="mb-3">
+                {!!historyItem && (
+                  <>
+                    {historyItem.paid && historyItem.grossValue && (
+                      <TooltipItem
+                        color={ w.globals.colors[0] }
+                        label={ w.globals.seriesNames[0] }
+                      >
+                        <Text>
+                          {formatCurrency(historyItem.grossValue)}
+                        </Text>
+                        <Growth>
+                          {formatNumber(historyItem.grossGrowth)}%
+                        </Growth>
+                        <Growth>
+                          {formatCurrency(historyItem.grossValueIncomeAccumulated)}
+                        </Growth>
+                      </TooltipItem>
+                    )}
+
+                    {!historyItem.paid && historyItem.grossValue && (
+                      <TooltipItem
+                        color={ w.globals.colors[2] }
+                        label={ w.globals.seriesNames[2] }
+                      >
+                        <Text>
+                          {formatCurrency(historyItem.grossValue)}
+                        </Text>
+                        <Growth>
+                          {formatNumber(historyItem.grossGrowth)}%
+                        </Growth>
+                        <Growth>
+                          {formatCurrency(historyItem.grossValueIncomeAccumulated)}
+                        </Growth>
+                      </TooltipItem>
+                    )}
+
+                    <TooltipItem
+                      color="rgb(239 68 68)"
+                      label="IOF"
+                    >
+                      <Text>
+                        {formatCurrency(historyItem.iofValue)}
+                      </Text>
+                    </TooltipItem>
+
+                    <TooltipItem
+                      color="rgb(239 68 68)"
+                      label="IR"
+                    >
+                      <Text>
+                        {formatCurrency(historyItem.irValue)}
+                      </Text>
+                    </TooltipItem>
+
+                    {historyItem.paid && historyItem.netValue && (
+                      <TooltipItem
+                        color={ w.globals.colors[1] }
+                        label={ w.globals.seriesNames[1] }
+                      >
+                        <Text>
+                          {formatCurrency(historyItem.netValue)}
+                        </Text>
+                        <Growth>
+                          {formatNumber(historyItem.netGrowth)}%
+                        </Growth>
+                        <Growth>
+                          {formatCurrency(historyItem.netValueIncomeAccumulated)}
+                        </Growth>
+                      </TooltipItem>
+                    )}
+
+                    {!historyItem.paid && historyItem.netValue && (
+                      <TooltipItem
+                        color={ w.globals.colors[3] }
+                        label={ w.globals.seriesNames[3] }
+                      >
+                        <Text>
+                          {formatCurrency(historyItem.netValue)}
+                        </Text>
+                        <Growth>
+                          {formatNumber(historyItem.netGrowth)}%
+                        </Growth>
+                        <Growth>
+                          {formatCurrency(historyItem.netValueIncomeAccumulated)}
+                        </Growth>
+                      </TooltipItem>
+                    )}
+                  </>
+                )}
+              </div>
+            </>
+          )
+        }
+      }
+    }),
+    [data?.history]
+  )
+
+  const series = useMemo<ApexAxisChartSeries>(
+    () => [
+      {
+        name: 'Valor bruto',
+        data: map(data?.history, item => item.paid ? item.grossValue : null)
+        // color: 'blue'
+      },
+      {
+        name: 'Valor líquido',
+        data: map(data?.history, item => item.paid ? item.netValue : null)
+        // color: 'green'
+      },
+      {
+        name: 'Valor bruto estimado',
+        data: map(data?.history, item => item.paid ? null : item.grossValue)
+        // color: 'red'
+      },
+      {
+        name: 'Valor líquido estimado',
+        data: map(data?.history, item => item.paid ? null : item.netValue)
+        // color: 'yellow'
+      }
+    ],
+    [data?.history]
+  )
 
   return (
-    <Modal.Root open={ open } close={ close }>
+    <Modal.Root open={ open } close={ close } className="max-w-7xl">
       <Modal.Title>
-        Histórico de Investimento
+        Histórico do Investimento
       </Modal.Title>
 
       {error ?? (
         <Modal.Description>
-          {isLoading
-            ? (
-              <div className="flex justify-center mt-8">
-                <CircleNotch
-                  weight="bold"
-                  className="animate-spin text-cyan-300"
-                  size={ 40 }
-                />
-              </div>
-              )
-            : (
-              <LineChart
-                showLabel
-                title="Concurrent lines"
-                xType="time"
-                toolboxTooltip={ { saveAsImage: 'saving' } }
-                dateFormat="yyyy-MM"
-                colors={ ['red', 'green', 'blue'] }
-                data={ [
-                  {
-                    name: 'top line',
-                    values: [
-                      {
-                        label: '2019-01',
-                        result: 10
-                      },
-                      {
-                        label: '2019-02',
-                        result: 40
-                      },
-                      {
-                        label: '2019-03',
-                        result: 30
-                      },
-                      {
-                        label: '2019-04',
-                        result: 20
-                      },
-                      {
-                        label: '2019-05',
-                        result: 40
-                      },
-                      {
-                        label: '2019-06',
-                        result: 50
-                      },
-                      {
-                        label: '2019-07',
-                        result: 15
-                      },
-                      {
-                        label: '2019-08',
-                        result: 70
-                      },
-                      {
-                        label: '2019-09',
-                        result: 80
-                      },
-                      {
-                        label: '2019-10',
-                        result: 90
-                      },
-                      {
-                        label: '2019-11',
-                        result: 70
-                      },
-                      {
-                        label: '2019-12',
-                        result: 80
-                      },
-                      {
-                        label: '2019-01',
-                        result: 90
-                      }
-                    ]
-                  },
-                  {
-                    name: 'bottom line',
-                    values: [
-                      {
-                        label: '2019-01',
-                        result: 1
-                      },
-                      {
-                        label: '2019-02',
-                        result: 4
-                      },
-                      {
-                        label: '2019-03',
-                        result: 3
-                      },
-                      {
-                        label: '2019-04',
-                        result: 2
-                      },
-                      {
-                        label: '2019-05',
-                        result: 4
-                      },
-                      {
-                        label: '2019-06',
-                        result: 5
-                      },
-                      {
-                        label: '2019-07',
-                        result: 1
-                      },
-                      {
-                        label: '2019-08',
-                        result: 7
-                      },
-                      {
-                        label: '2019-09',
-                        result: 8
-                      },
-                      {
-                        label: '2019-10',
-                        result: 9
-                      },
-                      {
-                        label: '2019-11',
-                        result: 70
-                      },
-                      {
-                        label: '2019-12',
-                        result: 80
-                      },
-                      {
-                        label: '2019-01',
-                        result: 90
-                      }
-                    ]
-                  }
-                ] }
-              />
-              )}
+          <div className="py-6">
+            {isLoading
+              ? (
+                <div className="flex justify-center">
+                  <CircleNotch
+                    weight="bold"
+                    className="animate-spin text-cyan-300"
+                    size={ 40 }
+                  />
+                </div>
+                )
+              : (
+                <div>
+                  <ApexChart
+                    options={ options }
+                    series={ series }
+                    height={ 350 }
+                    type="area"
+                  />
+                </div>
+                )}
+          </div>
         </Modal.Description>
       )}
-
-      <Modal.Footer>
-        <Button onClick={ close }>
-          Fechar
-        </Button>
-      </Modal.Footer>
-
     </Modal.Root>
   )
 }
