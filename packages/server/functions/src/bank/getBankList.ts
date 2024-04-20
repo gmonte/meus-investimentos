23122db45361c14ba7ad660559a2e51d1c7fd986
Guilherme Monte <gmonte@inf.ufsm.br>
2024-04-20 14:36:17 -0300

    feat(banks): added orderBy name
    affects: @meus-investimentos/server

--- packages/server/functions/src/bank/getBankList.ts
+++ packages/server/functions/src/bank/getBankList.ts
@@ -4,7 +4,9 @@ import { COLLECTIONS } from '../constants'
 import { BankDocument } from '../types'
 
 export const getBankList = async (db: admin.firestore.Firestore): Promise<BankDocument[]> => {
-  const banks = await db.collection(COLLECTIONS.BANKS).get()
+  const banks = await db.collection(COLLECTIONS.BANKS)
+    .orderBy('name', 'asc')
+    .get()
 
   return banks.docs.map(item => ({
     ...(item.data() as BankDocument),
